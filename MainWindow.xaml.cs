73598695aba5fa86bf1be06bca1a12a9495004b7
sasha15455 <sasha15452@gmail.com>
2021-06-10 10:45:23 +0300

    Items control was added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,39 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CustomGrid
+namespace ImagePanelWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<UIElement> itemsList;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            itemsList = new();
+
+            FillItemsList();
+
+            panelItemsControl.ItemsSource = itemsList;
+
+            //customImagePanel.ItemsList = itemsList;
+
+        }
+        private void FillItemsList()
+        {
+            itemsList.Add(this.FindResource("imageQ") as Image);
+            itemsList.Add(this.FindResource("imageQwe") as Image);
+            itemsList.Add(new Button() { Width = 30, Height = 120, Background = new SolidColorBrush(Colors.Black) });
+            itemsList.Add(new Button() { Width = 30, Height = 120, Background = new SolidColorBrush(Colors.Red) });
+            itemsList.Add(new Button() { Width = 120, Height = 15, Background = new SolidColorBrush(Colors.Yellow) });
+            itemsList.Add(new Button() { Width = 120, Height = 15, Background = new SolidColorBrush(Colors.Blue) });
+            itemsList.Add(new Button() { Width = 120, Height = 15, Background = new SolidColorBrush(Colors.Cyan) });
+            itemsList.Add(new Button() { Width = 30, Height = 120, Background = new SolidColorBrush(Colors.Orange) });
+            itemsList.Add(new Button() { Width = 120, Height = 20, Background = new SolidColorBrush(Colors.LightGray) });
         }
     }
 }
